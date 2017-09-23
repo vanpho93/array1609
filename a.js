8072a37d515cef480987cf4473122faa738c0917
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-23 13:50:29 +0700

    review

--- a.js
+++ a.js
@@ -13,3 +13,10 @@ const arrPerson = [
 ];
 
 // ['Teo', 'Ti', 'Tun']
+// console.log(arrPerson.map(e => e.name));
+
+// in ra mang cua name cua cac phan tu co age > 12 // ['Ti', 'Tun']
+const ageBiggerThan12 = arrPerson.filter(e => e.age > 12);
+console.log(ageBiggerThan12.map(e => e.name));
+
+console.log(arrPerson.filter(e => e.age > 12).map(e => e.name));
