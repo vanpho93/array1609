dd4d9f37ba7005dec5d53953b4a7468c32195601
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-23 15:12:32 +0700

    set and get

--- h.js
+++ h.js
@@ -3,6 +3,17 @@ class Person {
         this.name = name;
         this.age = age;
     }
+
+    get birthYear() {
+        return 2017 - this.age;
+    }
+
+    set birthYear(value) {
+        this.age = 2017 - value;
+    }
 }
 
 const teo = new Person('Teo', 10);
+// console.log(teo.birthYear);
+teo.birthYear = 2005;
+console.log(teo.age);
