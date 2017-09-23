7007c0323220be3217ef676553d70e67bed789c7
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-23 13:38:16 +0700

    init

---
+++ a.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(name, age, height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+}
+
+const arrPerson = [
+    new Person('Teo', 10, 160),
+    new Person('Ti', 15, 150),
+    new Person('Tun', 14, 180)
+];
+
+// ['Teo', 'Ti', 'Tun']
